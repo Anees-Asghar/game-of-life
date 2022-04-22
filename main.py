10d107dc3d37d95d0b55ce9bbd08963ba24efa2e
Anees Asghar <aneesasgharsyed@gmail.com>
2022-04-22 16:46:26 +0100

    Allow loading saved board state / clean up code

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import os
+import sys
 import time
 import random
 
-
-THRESHOLD = 0.7 # percentage of dead cells in initial state of the board
+LIVE_CELL_CHAR = '+'
+DEAD_CELL_CHAR = ' '
+SLEEP_TIME = 0.5 # dictates how long before the next generation is loaded on the terminal (seconds) 
+DEAD_CELL_PER = 0.8 # percentage of dead cells in initial state of the board
 
 
 def random_board(height, width):
     """
         Return a board of size height x width where each cell is either 0 (dead) or 1 
-        (living). The ratio of dead to living cells is defined by THRESHOLD constant.
+        (live). The ratio of dead to live cells is defined by the constant THRESHOLD.
     """
     board = []
     for _ in range(height):
-        row = [1 if random.random() > THRESHOLD else 0 for _ in range(width)]
+        row = [1 if random.random() > DEAD_CELL_PER else 0 for _ in range(width)]
         board.append(row)
     return board
 
@@ -24,7 +27,7 @@ def render_board(board):
     """
     board_str = ""
     for row in board:
-        board_str += "".join(['#' if cell else ' ' for cell in row])
+        board_str += "".join([LIVE_CELL_CHAR if cell else DEAD_CELL_CHAR for cell in row])
         board_str += '\n'
     print(board_str)
 
@@ -38,9 +41,9 @@ def next_board_state(board):
     for r, row in enumerate(board):
         new_row = []
         for c, cell in enumerate(row):
-            count = living_neighbor_count(board, (r, c))
-            if cell == 1: # if living cell
-                # acording to the rules of game of life, the only case when a living cell 
+            count = live_neighbor_count(board, (r, c))
+            if cell == 1: # if live cell
+                # acording to the rules of game of life, the only case when a live cell 
                 # doesn't die off is when it has the right numbers of neighbors, i.e, 2 or 3
                 new_cell = 0 if count not in [2, 3] else 1
                 new_row.append(new_cell)
@@ -52,9 +55,9 @@ def next_board_state(board):
     return next_state
 
 
-def living_neighbor_count(board, cell_index):
+def live_neighbor_count(board, cell_index):
     """
-        Return the count of living neighbors of a cell, provided the index 
+        Return the count of live neighbors of a cell, provided the index 
         of that cell.
     """
     r, c = cell_index
@@ -82,18 +85,56 @@ def living_neighbor_count(board, cell_index):
     for nr, nc in neighbors:
         neighbor_cells.append(board[nr][nc])
 
-    living_count = sum(neighbor_cells) # count the living neighbors
+    live_count = sum(neighbor_cells) # count the live neighbors
 
-    return living_count
+    return live_count
 
 
-if __name__ == "__main__":
-    board = random_board(20, 60) # initial board state
+def save_board(board, filename="last_image"):
+    """
+        Save board state in a file for later use.
+    """
+    with open(f"saved_states/{filename}", 'w') as f:
+        lines = []
+        for row in board:
+            line = ''.join([str(i) for i in row])
+            lines.append(line)
+        f.write('\n'.join(lines))
+
+
+def load_board(filename):
+    """
+        Load a saved board state.
+    """
+    board = []
+
+    with open(f"saved_states/{filename}", 'r') as f:
+        lines = f.read().split('\n')
+        for line in lines:
+            row = [int(i) for i in line]
+            board.append(row)
+
+    return board
+
+
+def main():
+    """
+        Loads an initial board state and runs the game of life.
+    """
+    if len(sys.argv) > 1:
+        board = load_board(filename=sys.argv[1])
+    else:
+        board = random_board(20, 60)
+        save_board(board)
 
     clear_cmd = 'cls' if os.name == 'nt' else 'clear'
 
     while True:
         os.system(clear_cmd) # clear terminal
         render_board(board)
-        time.sleep(0.3)
-        board = next_board_state(board)
+        time.sleep(SLEEP_TIME)
+        board = next_board_state(board) 
+
+
+if __name__ == "__main__":
+    main()
